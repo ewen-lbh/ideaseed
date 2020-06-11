cf28eaec38054f7e11c9f0e4746e64936cc1c5ac
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2020-06-11 23:40:16 +0200

    ➖➕ Use docopt instead of urfave/cli

--- go.mod
+++ go.mod
@@ -2,4 +2,6 @@ module github.com/ewen-lbh/ideasprout
 
 go 1.14
 
-require github.com/urfave/cli/v2 v2.2.0
+require (
+	github.com/docopt/docopt-go v0.0.0-20180111231733-ee0de3bc6815
+)
--- main.go
+++ main.go
@@ -2,13 +2,28 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
 
-	"github.com/urfave/cli/v2"
+	"github.com/docopt/docopt-go"
 )
 
 func main() {
+	usage := `Do you have ideas suddenly and just wished you could catch them as fast as possible, as to not loose them, without having to interrupt what you were doing?
+
+Usage:
+	ideasprout [options] IDEA...
+
+Options:
+	-g 	--gh=[[OWNER/]REPO]     	  Put the idea to github. If no argument is given, the card is added to one of the user's projects.
+	-p 	--project=PROJECT-NAME  	  Select which project to put the project too. Can only be used with --gh.
+	-c 	--column=COLUMN NAME|INDEX    Select which column of the project to use. Can only be used with --gh.
+		--create-columns        	  Prompt to create non-existant columns specified with --columns.
+	-d 	--color=COLOR           	  Chooses which color to use for Google Keep cards. Cannot be used with --gh. COLOR can be one of: bl[ue], br[own], d[arkblue], gra[y], gre[en], o[range], pi[nk], pu[rple], r[ed], t[eal], w[hite], y[ellow],
+	-t 	--tag=TAG NAME          	  Adds tags to the Google Keep card. Cannot be used with --gh.
+		--create-tags           	  Prompt to create non-existant tags specified with --tag.
+	-? 	--prompt-mode           	  Prompts you for the above options when they are not provided.
+`
+
+	opts, _ := docopt.ParseDoc(usage)
 	app := &cli.App{
 		Name:  "ideasprout",
 		Usage: "Swiftly catch that idea and get back to what you were doing",
