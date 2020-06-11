931763e2a6c7afa2dd080e855f39d1aa980f979f
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2020-06-11 23:40:46 +0200

    ✨ Implement color name validation/expansion

--- main.go
+++ main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"strings"
 	"fmt"
 
 	"github.com/docopt/docopt-go"
@@ -24,17 +26,57 @@ Options:
 `
 
 	opts, _ := docopt.ParseDoc(usage)
-	app := &cli.App{
-		Name:  "ideasprout",
-		Usage: "Swiftly catch that idea and get back to what you were doing",
-		Action: func(c *cli.Context) error {
-			fmt.Println("-- Work in progress :) --")
-			return nil
-		},
+	// Get the color option
+	color, _ := opts.String("--color")
+	// If we have specified it
+	if color != "" {
+		// Try to get the full color name (to handle eg. dar => darkblue)
+		var err error
+		color, err = expandColorName(color)
+		// Show the error
+		if err != nil {
+			fmt.Println(err)
+		// If we successfully expanded it, print it (for now!)
+		} else {
+			println(color)
+		}
 	}
 
-	err := app.Run(os.Args)
-	if err != nil {
-		log.Fatal(err)
+}
+
+func expandColorName(color string) (string, error) {
+	// All possible color names
+	colorNames := []string{
+		"blue",
+		"brown",
+		"darkblue",
+		"gray",
+		"green",
+		"orange",
+		"pink",
+		"purple",
+		"red",
+		"teal",
+		"white",
+		"yellow",
 	}
+	// Initialize the array of matches
+	matchingColorNames := []string{}
+	// Filter `colorNames` to only get the color names that start with `color`
+	for _, colorName := range colorNames {
+		if strings.HasPrefix(colorName, color) {
+			matchingColorNames = append(matchingColorNames, colorName)
+		}
+	}
+	// If we have exactly _one_ element of `colorNames` that matches, we return the only
+	// element: it's a match!
+	if len(matchingColorNames) == 1 {
+		return matchingColorNames[0], nil
+	}
+	// If we had multiple choice, the given color was ambiguous.
+	if len(matchingColorNames) > 1 {
+		return "", errors.New("Ambiguous color shorthand '" + color + "': could be one of: " + strings.Join(matchingColorNames, ", "))
+	}
+	// If we had an empty array, then the given color name was incorrect.
+	return "", errors.New("Unvalid color " + color)
 }
