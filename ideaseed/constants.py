bc87e6a69f362fe6adce78860a5904425dbebc42
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2020-06-19 11:01:32 +0200

    🔖 Bump version to 0.4.1

--- ideaseed/constants.py
+++ ideaseed/constants.py
@@ -2,7 +2,7 @@ from typing import *
 from flatten_list.flatten_list import flatten
 from semantic_version import Version
 
-VERSION = Version("0.4.0")
+VERSION = Version("0.4.1")
 
 RELEASES_RSS_URL = "https://pypi.org/rss/project/ideaseed/releases.xml"
 
