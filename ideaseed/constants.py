63d43bd191821c543cfcf2b5456ba83cd7f3b080
Ewen Le Bihan <ewen.lebihan7@gmail.com>
2020-06-24 18:13:53 +0200

    🔖 Release 0.10.0

--- ideaseed/constants.py
+++ ideaseed/constants.py
@@ -2,7 +2,7 @@ from typing import *
 from flatten_list.flatten_list import flatten
 from semantic_version import Version
 
-VERSION = Version("0.9.1")
+VERSION = Version("0.10.0")
 
 RELEASES_RSS_URL = "https://pypi.org/rss/project/ideaseed/releases.xml"
 
